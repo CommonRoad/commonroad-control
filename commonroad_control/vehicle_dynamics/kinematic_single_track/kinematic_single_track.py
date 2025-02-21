43ab3e583b4f9820f1aa7ac4cb0cd9a0d2aa1b98
Lukas Schäfer <ga42mab@mytum.de>
2025-02-21 14:53:01 +0100

    added vehicle parameter

--- commonroad_control/vehicle_dynamics/kinematic_single_track/kinematic_single_track.py
+++ commonroad_control/vehicle_dynamics/kinematic_single_track/kinematic_single_track.py
@@ -4,16 +4,17 @@ from math import atan, tan, sin, cos
 from commonroad_control.vehicle_dynamics.input_interface import InputInterface
 from commonroad_control.vehicle_dynamics.state_interface import StateInterface
 from commonroad_control.vehicle_dynamics.vehicle_model_interface import VehicleModelInterface
+from commonroad_control.vehicle_parameters.vehicle_parameters import VehicleParameters
 
 
 class KinematicSingleStrack(VehicleModelInterface):
-    def __init__(self):
+    def __init__(self, params: VehicleParameters):
         # init base class
         super().__init__(nx, nu)
 
-        # parameters
-        params = params(vehicle_id)
+        # set vehicle parameters
         self._l_wb = params.l_wb
+        self._l_r = params.l_r
 
     def _dynamics(self, x, u):
         " TODO: add dynaimcs function"
