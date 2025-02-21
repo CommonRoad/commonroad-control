e53b719f61dc5de6795b063a85036369477f9cc6
Lukas Schäfer <ga42mab@mytum.de>
2025-02-21 11:45:37 +0100

    add KST class

---
+++ commonroad_control/vehicle_models/kinematic_single_track/kinematic_single_track.py
@@ -0,0 +1,14 @@
+from commonroad_control.vehicle_models.input_interface import InputInterface
+from commonroad_control.vehicle_models.state_interface import StateInterface
+from commonroad_control.vehicle_models.vehicle_model_interface import VehicleModelInterface
+
+
+class KinematicSingleStrack(VehicleModelInterface):
+    def __init__(self):
+
+
+
+    def _dynamics(self, x, u):
+        " TODO: add dynaimcs function"
+
+
