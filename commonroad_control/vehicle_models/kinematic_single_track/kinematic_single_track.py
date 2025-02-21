4a5d85e489f719d8b7aacc00bec99c921768669c
Lukas Schäfer <ga42mab@mytum.de>
2025-02-21 14:08:30 +0100

    added dynamics for kst claSS

--- commonroad_control/vehicle_models/kinematic_single_track/kinematic_single_track.py
+++ commonroad_control/vehicle_models/kinematic_single_track/kinematic_single_track.py
@@ -1,3 +1,6 @@
+from math import atan, tan, sin, cos
+
+
 from commonroad_control.vehicle_models.input_interface import InputInterface
 from commonroad_control.vehicle_models.state_interface import StateInterface
 from commonroad_control.vehicle_models.vehicle_model_interface import VehicleModelInterface
@@ -5,10 +8,36 @@ from commonroad_control.vehicle_models.vehicle_model_interface import VehicleMod
 
 class KinematicSingleStrack(VehicleModelInterface):
     def __init__(self):
+        # init base class
+        super().__init__(nx, nu)
 
-
+        # parameters
+        params = params(vehicle_id)
+        self._l_wb = params.l_wb
 
     def _dynamics(self, x, u):
         " TODO: add dynaimcs function"
+
+        v = x(2)
+        a = x(3)
+        psi = x(4)
+        delta = x(5)
+
+        # compute slip angle
+        beta = atan(tan(delta)*self._l_r/self._l_wb)
+
+        x_dot = v*cos(psi + beta)
+        y_dot = v*sin(psi + beta)
+        v_dot = a
+        a_dot = u(1)
+        psi_dot = v*sin(beta) / self._l_r
+        delta_dot = u(2)
+
+        f = [x_dot, y_dot, v_dot, a_dot, psi_dot, delta_dot]
+
+        return f
+
+
+
 
 
