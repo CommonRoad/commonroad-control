a0fe8b94ac97597c3f20aa0fd8f084b5a54f9269
Lukas Schäfer <ga42mab@mytum.de>
2025-03-03 22:12:48 +0100

    double integrator: disretization using control toolbox

--- commonroad_control/vehicle_dynamics/double_integrator/double_integrator.py
+++ commonroad_control/vehicle_dynamics/double_integrator/double_integrator.py
@@ -1,7 +1,8 @@
 import casadi as cas
 import numpy as np
-import scipy as sp
 from typing import Tuple, Union
+from control import StateSpace
+
 
 from commonroad_control.vehicle_dynamics.vehicle_model_interface import VehicleModelInterface
 from commonroad_control.vehicle_parameters.vehicle_parameters import VehicleParameters
@@ -12,16 +13,29 @@ from commonroad_control.vehicle_dynamics.double_integrator.di_input import DIInp
 class DoubleIntegrator(VehicleModelInterface):
     def __init__(self, params: VehicleParameters, dt: float):
 
-        # continuous-time dynamics matrices
-        self._sys_mat = np.array([[0, 0, 1, 0], [0, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0]], dtype=float)
-        self._input_mat = np.array([[0, 0], [0, 0], [1, 0], [0, 1]], dtype=float)
-
-        # discrete-time dynamics matrices
-        self._sys_mat_dt = np.array([[1, 0, dt, 0], [0, 1, 0, dt], [0, 0, 1, 0], [0, 0, 0, 1]], dtype=float)
-        self._input_mat_dt = np.array([[0.5*dt**2, 0], [0, 0.5*dt**2], [dt, 0], [0, dt]], dtype=float)
+        self._sys_mat, self._input_mat = self._system_matrices()
 
         # init base class
         super().__init__(nx=DIStateIndices.dim, nu=DIInputIndices.dim, dt=dt)
+
+    @staticmethod
+    def _system_matrices() -> Tuple[np.array, np.array]:
+        """
+
+        :return:
+        """
+
+        # system matrix
+        sys_mat = np.zeros((DIStateIndices.dim, DIStateIndices.dim))
+        sys_mat[DIStateIndices.position_long, DIStateIndices.velocity_long] = 1.0
+        sys_mat[DIStateIndices.position_lat, DIStateIndices.velocity_lat] = 1.0
+
+        # input matrix
+        input_mat = np.zeros((DIStateIndices.dim, DIInputIndices.dim))
+        input_mat[DIStateIndices.velocity_long, DIInputIndices.acceleration_long] = 1.0
+        input_mat[DIStateIndices.velocity_lat, DIInputIndices.acceleration_lat] = 1.0
+
+        return sys_mat, input_mat
 
     def simulate_forward(self, x: DIState, u: DIInput) -> DIState:
         pass
@@ -38,6 +52,15 @@ class DoubleIntegrator(VehicleModelInterface):
 
         return self._sys_mat@x + self._input_mat@u
 
+    def input_bounds(self) -> Tuple[DIInput, DIInput]:
+
+        # lower bound
+        u_lb = DIInput(acceleration_long=-9.0, acceleration_lat=-2.0)
+        # upper bound
+        u_ub = DIInput(acceleration_long=5.0, acceleration_lat=2.0)
+
+        return u_lb, u_ub
+
     def linearize(self, x: DIState, u: DIInput) -> Tuple[DIState, np.array, np.array]:
         pass
 
@@ -53,14 +76,21 @@ class DoubleIntegrator(VehicleModelInterface):
         :return:
         """
 
+        #
+        LTI_ct = StateSpace(self._sys_mat, self._input_mat,
+                            np.eye(self._nx), np.zeros((self._nx, self._nu)))
+        LTI_dt = LTI_ct.sample(Ts=self._dt)
+        sys_mat_dt = LTI_dt.A
+        input_mat_dt = LTI_dt.B
+
         xk = cas.SX.sym("xk", self._nx, 1)
         uk = cas.SX.sym("uk", self._nu, 1)
 
         # discrete-time dynamics
-        x_next = cas.Function("dynamics_dt", [xk, uk], [self._sys_mat_dt@xk + self._input_mat_dt@uk])
+        x_next = cas.Function("dynamics_dt", [xk, uk], [sys_mat_dt@xk + input_mat_dt@uk])
 
         # Jacobians of the discretized dynamics
-        jac_x = cas.Function("jac_dynamics_dt_x", [xk, uk], [self._sys_mat_dt])
-        jac_u = cas.Function("jac_dynamics_dt_u", [xk, uk], [self._input_mat_dt])
+        jac_x = cas.Function("jac_dynamics_dt_x", [xk, uk], [sys_mat_dt])
+        jac_u = cas.Function("jac_dynamics_dt_u", [xk, uk], [input_mat_dt])
 
         return x_next, jac_x, jac_u
