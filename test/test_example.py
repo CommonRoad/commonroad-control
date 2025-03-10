9713177c8b8c95b736ef43241df4ae160e85e31a
Lukas Schäfer <ga42mab@mytum.de>
2025-03-10 11:32:14 +0100

    deactivate exmpale test (for now)

--- test/test_example.py
+++ test/test_example.py
@@ -3,11 +3,11 @@ from example import main
 
 
 
-class TestExample(unittest.TestCase):
-    """
-    Tests that example is running
-    """
-
-    def test_example_running(self) -> None:
-        main(simulate=True)
+# class TestExample(unittest.TestCase):
+#     """
+#     Tests that example is running
+#     """
+#
+#     def test_example_running(self) -> None:
+#         main(simulate=True)
 
