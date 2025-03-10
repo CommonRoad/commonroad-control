2baea4482eb372fa3a6c311095023e1973a2b266
Lukas Schäfer <ga42mab@mytum.de>
2025-03-10 10:30:13 +0100

    fix MPC

--- commonroad_control/vehicle_dynamics/double_integrator/double_integrator.py
+++ commonroad_control/vehicle_dynamics/double_integrator/double_integrator.py
@@ -1,14 +1,14 @@
 import casadi as cas
 import numpy as np
 from typing import Tuple, Union
-# from control import StateSpace
-import scipy.signal as ss
+import scipy.signal as scsi
 
 
 from commonroad_control.vehicle_dynamics.vehicle_model_interface import VehicleModelInterface
 from commonroad_control.vehicle_parameters.vehicle_parameters import VehicleParameters
 from commonroad_control.vehicle_dynamics.double_integrator.di_state import DIState, DIStateIndices
 from commonroad_control.vehicle_dynamics.double_integrator.di_input import DIInput, DIInputIndices
+from commonroad_control.vehicle_dynamics.double_integrator.di_sit_factory import DISITFactory
 
 
 class DoubleIntegrator(VehicleModelInterface):
@@ -54,6 +54,10 @@ class DoubleIntegrator(VehicleModelInterface):
         return self._sys_mat@x + self._input_mat@u
 
     def input_bounds(self) -> Tuple[DIInput, DIInput]:
+        """
+
+        :return:
+        """
 
         # lower bound
         u_lb = DIInput(acceleration_long=-9.0, acceleration_lat=-2.0)
@@ -61,6 +65,31 @@ class DoubleIntegrator(VehicleModelInterface):
         u_ub = DIInput(acceleration_long=5.0, acceleration_lat=2.0)
 
         return u_lb, u_ub
+
+    def state_bounds(self) -> Tuple[np.array, np.array, np.array, np.array]:
+        """
+
+        :return:
+        """
+        # lower bound lb <= mat_lb*x
+        mat_lb = np.zeros((2, DIStateIndices.dim), dtype=float)
+        mat_lb[0, DIStateIndices.velocity_long] = 1.0
+        mat_lb[1, DIStateIndices.velocity_lat] = 1.0
+
+        lb = np.zeros((DIStateIndices.dim, 1), dtype=float)
+        lb[DIStateIndices.velocity_long] = 0.0
+        lb[DIStateIndices.velocity_lat] = -2.0
+        lb = mat_lb @ lb
+
+        # uppber bound mat_ub*x <= ub
+        mat_ub = mat_lb
+
+        ub = np.zeros((DIStateIndices.dim, 1), dtype=float)
+        ub[DIStateIndices.velocity_long] = 10.0
+        ub[DIStateIndices.velocity_lat] = 2.0
+        ub = mat_ub @ ub
+
+        return mat_lb, lb, mat_ub, ub
 
     def linearize(self, x: DIState, u: DIInput) -> Tuple[DIState, np.array, np.array]:
         pass
@@ -77,24 +106,19 @@ class DoubleIntegrator(VehicleModelInterface):
         :return:
         """
 
-        #
-        # LTI_ct = StateSpace(self._sys_mat, self._input_mat,
-        #                     np.eye(self._nx), np.zeros((self._nx, self._nu)))
-        # LTI_dt = LTI_ct.sample(Ts=self._dt)
-        LTI_ct = ss.lti(self._sys_mat, self._input_mat,
+        # compute matrices of discrete-time LTI system
+        lit_ct = scsi.lti(self._sys_mat, self._input_mat,
                             np.eye(self._nx), np.zeros((self._nx, self._nu)))
-        LTI_dt = LTI_ct.to_discrete(dt=self._dt, method='zoh')
-
-        sys_mat_dt = LTI_dt.A
-        input_mat_dt = LTI_dt.B
-
-        xk = cas.SX.sym("xk", self._nx, 1)
-        uk = cas.SX.sym("uk", self._nu, 1)
+        lit_dt = lit_ct.to_discrete(dt=self._dt, method='zoh')
+        sys_mat_dt = lit_dt.A
+        input_mat_dt = lit_dt.B
 
         # discrete-time dynamics
+        xk = cas.SX.sym("xk", self._nx, 1)
+        uk = cas.SX.sym("uk", self._nu, 1)
         x_next = cas.Function("dynamics_dt", [xk, uk], [sys_mat_dt@xk + input_mat_dt@uk])
 
-        # Jacobians of the discretized dynamics
+        # Jacobians of the discrete-time dynamics
         jac_x = cas.Function("jac_dynamics_dt_x", [xk, uk], [sys_mat_dt])
         jac_u = cas.Function("jac_dynamics_dt_u", [xk, uk], [input_mat_dt])
 
--- test/controller/test_MPC.py
+++ test/controller/test_MPC.py
@@ -1,6 +1,7 @@
 import unittest
 
 import numpy as np
+import cvxpy as cp
 
 from commonroad_control.control.model_predictive_control.model_predictive_control import ModelPredictiveControl
 from commonroad_control.vehicle_dynamics.double_integrator.double_integrator import DoubleIntegrator
@@ -13,17 +14,21 @@ from commonroad_control.vehicle_dynamics.double_integrator.di_sit_factory import
 vehicle_model = DoubleIntegrator(params=BMW3seriesParams(), dt=0.1)
 
 # time
-horizon = 20
+horizon = 1
 delta_t = 0.1
 
 # cost matrices
 Q = np.eye(DIStateIndices.dim)
-R = np.eye(DIInputIndices.dim)
+R = np.zeros((DIInputIndices.dim, DIInputIndices.dim))
 P = np.eye(DIStateIndices.dim)
 
 # Define the initial state
 x0 = DIState(position_long=0.0, position_lat=2.0, velocity_long=5.0, velocity_lat=0.0)
-
+# initial state for 1-step - can be steered into the origin for delta t = 0.1
+u = -1.2
+pos = (-0.5*delta_t**2 + delta_t**2)*u
+v = (-delta_t)*u
+x0 = DIState(position_long=pos, velocity_long=v, position_lat=pos, velocity_lat=v)
 
 time_state = [kk for kk in range(horizon+1)]
 time_input = [kk for kk in range(horizon)]
@@ -31,21 +36,46 @@ sit_factory = DISITFactory()
 
 # reference trajectory
 x_ref_np = np.zeros((DIStateIndices.dim, horizon+1))
-x_ref = sit_factory.trajectory_from_numpy_array(traj_np=x_ref_np,mode='state',time=time_state,t_0=0,delta_t=delta_t)
+# x_ref_np[DIStateIndices.position_long,:] = [x0.velocity_long*kk*delta_t for kk in range(horizon+1)]
+x_ref = sit_factory.trajectory_from_numpy_array(traj_np=x_ref_np, mode='state', time=time_state,  t_0=0, delta_t=delta_t)
 u_ref_np = np.zeros((DIInputIndices.dim, horizon))
-u_ref = sit_factory.trajectory_from_numpy_array(traj_np=u_ref_np,mode='input',time=time_input,t_0=0,delta_t=delta_t)
+u_ref = sit_factory.trajectory_from_numpy_array(traj_np=u_ref_np, mode='input', time=time_input,t_0=0, delta_t=delta_t)
 
 # initial guess/ reference for linearization
 x_init_np = np.zeros((DIStateIndices.dim, horizon+1))
-x_init = sit_factory.trajectory_from_numpy_array(traj_np=x_init_np,mode='state',time=time_state,t_0=0,delta_t=delta_t)
+x_init = sit_factory.trajectory_from_numpy_array(traj_np=x_init_np, mode='state', time=time_state, t_0=0, delta_t=delta_t)
 u_init_np = np.zeros((DIInputIndices.dim, horizon))
-u_init = sit_factory.trajectory_from_numpy_array(traj_np=u_init_np,mode='input',time=time_input,t_0=0,delta_t=delta_t)
+u_init = sit_factory.trajectory_from_numpy_array(traj_np=u_init_np, mode='input', time=time_input, t_0=0, delta_t=delta_t)
 
 # instantiate model predictive controller
 solver = ModelPredictiveControl(vehicle_model, horizon, delta_t=delta_t, cost_xx=Q, cost_uu=R, cost_final=P)
 
 # Solve the optimal control problem
-x_sol, u_sol, iteration_history = solver._solve_ocp(x0, t_0 = 0.0, x_ref=x_ref, u_ref=u_ref, x_init=x_init, u_init=u_init)
+x_sol, u_sol, iteration_history = solver._solve_ocp(x0, t_0=0.0, x_ref=x_ref, u_ref=u_ref, x_init=x_init, u_init=u_init)
+
+var_x = cp.Variable((solver._nx, horizon+1))
+var_u = cp.Variable((solver._nu, horizon))
+
+par_x0 = cp.Parameter((solver._nx,))
+
+cost = 0
+
+_, A, B = vehicle_model.linearize_dt_at(np.zeros((4, 1)), np.zeros((2, 1)))
+
+# Constraints list
+constraints = [solver._x[:, 0] == par_x0]
+
+for kk in range(horizon):
+    cost += cp.sum_squares(var_x[:, kk + 1]) + cp.sum_squares(var_u[:, kk])
+    constraints += [var_x[:, kk + 1] == A @ var_x[:, kk] + B @ var_u[:, kk],
+                    cp.norm(var_u[:, kk], "inf") <= 1]
+
+# sums problem objectives and concatenates constraints.
+problem = cp.Problem(cp.Minimize(cost), constraints)
+
+par_x0.value = x0.convert_to_array()
+
+problem.solve(solver='CLARABEL', verbose=True)
 
 # Print the solution
 print("Optimal State Trajectory:\n", x_sol)
